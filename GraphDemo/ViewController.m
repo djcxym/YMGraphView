f321a650057ec04012a846510baf0a3bdbfd4355
djcxym <djcxym@126.com>
2016-07-25 18:01:20 +0800

    小改动

--- GraphDemo/ViewController.m
+++ GraphDemo/ViewController.m
@@ -10,7 +10,7 @@
 #import "YMGraphView.h"
 #import "GraphViewController.h"
 
-@interface ViewController ()<UITableViewDataSource,UITableViewDelegate,UIViewControllerAnimatedTransitioning,UINavigationControllerDelegate>
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
 
 @property (nonatomic,strong) NSArray *data;
 
@@ -18,7 +18,6 @@
 
 @property (nonatomic,strong) UITableView *tableView;
 
-@property (nonatomic,strong) UIPercentDrivenInteractiveTransition *interactivePopTransition;
 
 @end
 
@@ -28,17 +27,13 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor purpleColor];
     self.navigationItem.title = @"图表";
-    self.navigationController.delegate = self;
-//    _data = @[@"散点图",@"饼状图",@"柱状图",@"环状图",@"折线线",@"多条折线",@"函数"];
+    _data = @[@"散点图",@"饼状图",@"柱状图",@"环状图",@"折线线",@"多条折线",@"函数"];
     _style = @[@(YMGraphViewStyleScatter),@(YMGraphViewStylePie),@(YMGraphViewStyleHist),@(YMGraphViewStyleAnnular),@(YMGraphViewStyleLine),@(YMGraphViewStyleMultiLine),@(-1)];
-//    _tableView = [[UITableView alloc]initWithFrame:self.view.bounds];
-//    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
-//    _tableView.dataSource = self;
-//    _tableView.delegate = self;
-//    [self.view addSubview:_tableView];
-    
-    UIPanGestureRecognizer * panGes = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)];
-    [self.view addGestureRecognizer:panGes];
+    _tableView = [[UITableView alloc]initWithFrame:self.view.bounds];
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    [self.view addSubview:_tableView];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -63,73 +58,4 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-- (id<UIViewControllerAnimatedTransitioning>)
-navigationController:(UINavigationController *)navigationController
-animationControllerForOperation:(UINavigationControllerOperation)operation
-fromViewController:(UIViewController*)fromVC
-toViewController:(UIViewController*)toVC
-{
-//    if (operation == UINavigationControllerOperationPush) {
-//        return self;
-//    }
-//    return nil;
-    return self;
-}
-
-- (id <UIViewControllerInteractiveTransitioning>)navigationController:(UINavigationController*)navigationController
-                          interactionControllerForAnimationController:(id <UIViewControllerAnimatedTransitioning>)animationController
-{
-    return self.interactivePopTransition;
-}
-
-- (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext
-{
-    return 1;
-}
-
-- (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
-{
-    UIViewController* toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    UIViewController* fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-    [[transitionContext containerView] addSubview:toViewController.view];
-    toViewController.view.transform = CGAffineTransformMakeTranslation(320, 0);
-//    toViewController.view.transform = CGAffineTransformMakeScale(1, 1);
-    fromViewController.view.transform = CGAffineTransformIdentity;
-    [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
-        fromViewController.view.transform = CGAffineTransformMakeTranslation(-320, 0);
-        toViewController.view.transform = CGAffineTransformIdentity;
-    } completion:^(BOOL finished) {
-        fromViewController.view.transform = CGAffineTransformIdentity;
-        toViewController.view.transform = CGAffineTransformIdentity;
-        [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
-        
-    }];
-}
-
-- (void)panGesture:(UIPanGestureRecognizer *)gesture {
-    CGFloat xOff = [gesture translationInView:self.view].x;
-    CGFloat percent = fabs(xOff)/self.view.frame.size.width;
-    CGPoint location = [gesture locationInView:self.view];
-    if (gesture.state == UIGestureRecognizerStateBegan) {
-        if (location.x >  CGRectGetMidX(self.view.bounds)) {
-            self.interactivePopTransition = [[UIPercentDrivenInteractiveTransition alloc] init];
-            GraphViewController *graphVC = [[GraphViewController alloc]init];
-            graphVC.style = (YMGraphViewStyle)[_style[0] integerValue];
-            [self.navigationController pushViewController:graphVC animated:YES];
-        }
-    } else if (gesture.state == UIGestureRecognizerStateChanged) {
-        CGFloat d = (xOff / CGRectGetWidth(self.view.bounds)) * -1;
-        [self.interactivePopTransition updateInteractiveTransition:d];
-    } else if (gesture.state == UIGestureRecognizerStateEnded) {
-        if ([gesture velocityInView:self.view].x < 0) {
-            [self.interactivePopTransition finishInteractiveTransition];
-        } else {
-            [self.interactivePopTransition cancelInteractiveTransition];
-        }
-        self.interactivePopTransition = nil;
-    }
-}
-
-
 @end
